29ab8b8be6b5349449763f10a8a89f0e77cff53d
adam-bello <72825559+adam-bello@users.noreply.github.com>
2024-04-15 18:26:01 -0400

    push

--- App.js
+++ App.js
@@ -8,8 +8,10 @@ import UserRoutes from "./Users/routes.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import "dotenv/config";
+import QuizRoutes from './Kanbas/quizzes/routes.js';
+import QuestionRoutes from './Kanbas/questions/routes.js';
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+const CONNECTION_STRING = 'mongodb+srv://Just0717:Just0717@cluster0.nfmtkwb.mongodb.net/kanbas?retryWrites=true&w=majority&appName=Cluster0' || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
@@ -40,6 +42,8 @@ app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
 UserRoutes(app);
+QuizRoutes(app);
+QuestionRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000)
